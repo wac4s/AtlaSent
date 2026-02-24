3f952b8c2a65fb85510654c99514cf5fc6074351
wac4s <waqasahmadsultan.com>
2026-02-25 00:54:05 +0500

    ayo updated code bro

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-
+# ayo bro salamulaykum
 @app.route("/")
 def index():
     return render_template("index.html")
